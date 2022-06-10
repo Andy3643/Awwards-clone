c320a7b8fefc061bc7394c09686fd24c9b0c2392
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-06-10 22:48:18 +0300

    ft:create projects class model

--- myapp/models.py
+++ myapp/models.py
@@ -12,3 +12,25 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+    
+class Project(models.Model):
+    '''
+    Model for uploaded projects
+    '''
+    title = models.CharField(max_length=40)
+    description = models.TextField()
+    link = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='site_photos/')
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username} {self.title} Project'
+
+
+
+
+
+
+
+
