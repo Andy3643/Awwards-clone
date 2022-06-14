37f4b5445c83a6930feca6102c33d6cb4f8ccfff
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-06-15 02:08:56 +0300

    fix:app not deploying

--- awwards/settings.py
+++ awwards/settings.py
@@ -37,7 +37,7 @@ STATICFILES_DIRS = (
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY=config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
